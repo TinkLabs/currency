bd5426a81a2ab475c48556bad531dc31054db9b8
michael_yin <michael.yin@tinklabs.com>
2018-09-14 16:52:35 +0800

    dep ensure add github.com/Rhymond/go-money

--- handlers/import_packages.go
+++ handlers/import_packages.go
@@ -1,13 +1,13 @@
 package handlers
 
 import (
-	_ "gopkg.in/go-playground/validator.v9"
-
 	_ "github.com/TinkLabs/go-webservices/fixer"
 	_ "github.com/TinkLabs/go-webservices/fixer/convert"
 	_ "github.com/TinkLabs/go-webservices/fixer/latest"
 	_ "github.com/TinkLabs/go-webservices/fixer/symbols"
 	_ "github.com/TinkLabs/go-webservices/fixer/time-series"
 
+	_ "gopkg.in/go-playground/validator.v9"
 	_ "github.com/shopspring/decimal"
+	_ "github.com/Rhymond/go-money"
 )
